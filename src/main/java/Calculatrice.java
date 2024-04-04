78876700493a2b0d71cdf2485e99667ea816f557
Bousella Abderrahim <Bousella_Abderrahim@gmail.com>
2024-04-04 09:45:27 +0100

    Initial commit

---
+++ src/main/java/Calculatrice.java
@@ -0,0 +1,11 @@
+public class Calculatrice {
+    public int additionner(int a, int b) {
+        result = a + b ;
+        return result;
+    }
+    private int result ;
+
+    public int getState() {
+        return result;
+    }
+}
